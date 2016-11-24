a41a35e6406c62482632ce70a281fc634440ff34
Leuat <leuat@irio.co.uk>
2016-11-24 14:52:55 +0100

    OMP

--- scripts/remote_run.sh
+++ scripts/remote_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm output/$1_output.txt
-./Nougat likelihood data/mcmc_model.xyz data/sio2_bulk.xyz data/$1.txt output/$1_output.txt $2 
+./Nougat likelihood data/mcmc_model.xyz data/sio2_bulk.xyz data/$1.txt output/$1_output.txt $2 $3
