a4ebcce800a303ec8d6bd00413c4d08a1227d460
Leuat <leuat@irio.co.uk>
2017-01-23 15:21:59 +0100

    BumpGL

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,12 @@ echo   3: data file
 echo   4: parameter file
 echo   5: number of samples
 echo   6: number of random vectors
+echo   7: temperature 20
 rsync models/sio2_bulk.xyz models/$4.txt models/$3.xyz nicolaag@owl2:Fys2016/build/data/
 rsync remote_run.sh nicolaag@owl2:Fys2016/build/
 for run in {1..6}
 do
 	echo Starting likelihood with data: $4 and parameters $3 type $2
-    ssh nicolaag@owl$1 "cd Fys2016/build/;screen -d -m ./remote_run.sh $2 $4 $3 $5 $6"
+    ssh nicolaag@owl$1 "cd Fys2016/build/;screen -d -m ./remote_run.sh $2 $4 $3 $5 $6 $7"
     sleep 0.5
 done
--- scripts/remote_run.sh
+++ scripts/remote_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm output/$2_output.txt
-./Nougat likelihood $1 data/$3.xyz data/sio2_bulk.xyz data/$2.txt output/$2_output.txt $4 $5
+./Nougat likelihood $1 data/$3.xyz data/sio2_bulk.xyz data/$2.txt output/$2_output.txt $4 $5 $6
 # echo Starting likelihood with data: $2 and parameters $1
